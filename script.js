f567a42f25d8fdd00475756de0a323076a965391
maggiebarbie1995 <maggylangakush@gmail.com>
2020-05-03 22:35:38 +0300

    added the variables needed for calculation

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function wawa() {
     var date = document.getElementById('input_id').value;
     var gender = document.getElementById('gender').value;
+    date.toString();
+    var centuryDigits;
+    var yearDigits;
+    var month;
+    var dayDay;
+    var year;
