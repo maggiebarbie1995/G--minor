817f32cd6c9d96cef89543b870076d11c93d84b1
maggiebarbie1995 <maggylangakush@gmail.com>
2020-05-03 22:46:40 +0300

    added a formular for calculating birthweek

--- script.js
+++ script.js
@@ -21,4 +21,8 @@ function wawa() {
     var days;
     var maleNames;
     var femaleNames;
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']; 
+    femaleNames =['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var akanName;
       
