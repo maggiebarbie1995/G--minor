a44f2fbf768bef229320699cefadf94f21ac3bfa
maggiebarbie1995 <maggylangakush@gmail.com>
2020-05-03 17:58:46 +0300

    added title and links

---
+++ script.js
@@ -0,0 +1,31 @@
+function wawa() {
+  var date = document.getElementById('input_id').value;
+  var gender = document.getElementById('gender').value;
+  date.toString();
+  var centuryDigits;
+  var yearDigits;
+  var month;
+  var dayDay;
+  var year;
+  centuryDigits = parseInt(date.slice(0, 2));
+  yearDigits = parseInt(date.slice(2, 4));
+  month = parseInt(date.slice(5, 7));
+  dayDay = parseInt(date.slice(8, 10));
+  year = parseInt(date.slice(0, 4));    
+  if ((dayDay <= 0 || dayDay > 31) || (month <= 0 || month > 12)) {
+    alert("You entered invalid date.");
+  }
+  var birthWeek = ( ( (centuryDigits/4) -2*centuryDigits-1) + ((5*yearDigits/4) ) + ((26*(month+1)/10)) + dayDay ) % 7;
+  var birthWeekTrunc;
+  birthWeekTrunc = Math.trunc(birthWeek);
+  var days;
+  var maleNames;
+  var femaleNames;
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']; 
+  femaleNames =['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+  var akanName;
+  if (gender === "male"){ akanName = maleNames[birthWeekTrunc]; }
+  else { akanName = femaleNames[birthWeekTrunc]; }
+  document.getElementById('alert').innerHTML = 'Your akan name is: ' + akanName;
+}  
